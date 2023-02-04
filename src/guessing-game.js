9d98f41d8a3be6955e1ecd4d9236ba6c77b47541
Cl0udKiD <sasha.azarov8@gmail.com>
2023-02-04 21:30:52 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min_num=min
+        this.max_num=max
     }
 
     guess() {
-
+        console.log(this.max_num)
+        return Math.ceil((this.max_num+this.min_num)/2)
+        
     }
 
     lower() {
-
+        this.max_num=this.guess()
     }
 
     greater() {
-
+        this.min_num=this.guess()
     }
 }
 
